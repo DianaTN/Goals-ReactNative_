9d9fed35c762fd6cc720e728c78d33fe58e23106
DianaTN <alumno.247863@ies-azarquiel.es>
2024-01-16 09:27:32 +0100

    Maquetado

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Input your goal' />
+        <Button title='Input Goals' />
+      </View>
+
+      <View style={styles.goalsContainer}>
+        <Text>Your list of goals...</Text>
+      </View>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = new StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    paddingHorizontal: 15
   },
-});
+
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#48cae4",
+    paddingBottom: 20,
+    alignItems: "center",
+    flex: 1
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#48cae4",
+    width: "70%",
+    padding: 10
+  },
+
+  goalsContainer: {
+    flex: 14
+  }
+})
