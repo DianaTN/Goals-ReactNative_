e0fb7f1dc8b34705e8254bc555d7f746e0952ab2
DianaTN <alumno.247863@ies-azarquiel.es>
2024-01-17 10:49:24 +0100

    Handlers Creados

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+
 
 export default function App() {
+
+  // Declaramos el hook de estado, el cual es un array, con el componente "New Goal"
+  const [newGoal, setNewGoal] = useState("");
+  const [listaGoals, setListGoals] = useState([]);
+
+  function textChangeHandler(enteredText) {
+    setNewGoal(enteredText);
+    console.log(newGoal)
+  }
+
+  function addGoalHandler() {
+    setListGoals(myCurrentGoals => [...myCurrentGoals, newGoal])
+    console.log(listaGoals);
+  }
+
   return (
     <View style={styles.container}>
-
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Input your goal' />
-        <Button title='Input Goals' />
+        <TextInput
+          style={styles.textInput}
+          onChangeText={textChangeHandler}
+          placeholder='Input your goal'
+        />
+        <Button
+          title='Add Goal'
+          onPress={addGoalHandler}
+        />
       </View>
 
       <View style={styles.goalsContainer}>
